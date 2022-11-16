c0d7fdf8a3851f55628deb4190beab7bd076099e
Thyago Makiyama <thyago.makiyama@gmail.com>
2022-11-16 09:53:05 -0300

    feat: test to check quality gate

--- src/opa.js
+++ src/opa.js
@@ -1,4 +1,21 @@
 function opa() {
     return 'Opa! Bão demais?'
 }
+
+function test() {
+    return 'Opa! Bão demais?'
+}
+
+function anotherTest() {
+    return 'Opa! Bão demais?'
+}
+
+function vish() {
+    return 'Opa! Bão demais?'
+}
+
+function again() {
+    return 'Opa! Bão demais?'
+}
+
 module.exports = opa;
